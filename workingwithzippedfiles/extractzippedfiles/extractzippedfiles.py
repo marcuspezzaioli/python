f0e81d7dd3b72de461d466c60612d7e985a332fe
marcuspezzaioli <marcuspezzaioli@gmail.com>
2024-04-08 23:09:04 +0100

    tidy of extract zipped files

--- workingwithzippedfiles/extractzippedfiles/extractzippedfiles.py
+++ workingwithzippedfiles/extractzippedfiles/extractzippedfiles.py
@@ -3,8 +3,8 @@ from zipfile import ZipFile
 
 
 # Gets the directories of the zipped files, and the directory we will extract to.
-zipped_path = ".\zipped"
-extracted_path = ".\extracted"
+zipped_path = ".\\zipped"
+extracted_path = ".\\extracted"
 
 for file in os.listdir(zipped_path): # Loops through everything in the Zipped directory.
         with ZipFile(zipped_path + "/" + file, 'r') as zObject: 
